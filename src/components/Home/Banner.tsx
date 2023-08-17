eabbf506870d1c6b5a5d502714f43b8d7422ce77
Sanjay <sanjaymidhafunny@gmail.com>
2023-08-17 15:37:24 +0530

    Fixed spacing

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -24,7 +24,7 @@ const Banner = () => {
                 sx={{
                   paddingTop: '140px',
                   '@media(max-width:767px)': {
-                    paddingTop: '80px',
+                    paddingTop: '40px',
                   },
                 }}
               >
